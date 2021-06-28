90a04af1b4603b3e2a92ecc1610d16f90dc01ec8
ebuzer33 <ebuzer.tutuk@gmail.com>
2021-06-28 23:08:32 +0300

    First Commit

---
+++ src/test/java/StepDefinitions/loginSteps.java
@@ -0,0 +1,51 @@
+package StepDefinitions;
+
+import Pages.loginPage;
+import Utilities.BaseDriver;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+
+public class loginSteps {
+
+    loginPage lp=new loginPage();
+    @Given("^Navigate to website$")
+    public void navigateToWebsite()
+    {
+        WebDriver driver= BaseDriver.getDriver();
+        driver.get("http://zero.webappsecurity.com/");
+        driver.manage().window().maximize();
+    }
+
+    @And("^Click to Sign in button$")
+    public void clickToSignInButton()
+    {
+        lp.findAndClick("signInButton");
+    }
+
+    @And("^Enter username as \"([^\"]*)\" and password as \"([^\"]*)\"$")
+    public void enterUsernameAsAndPasswordAs(String login1, String password1)
+    {
+        lp.findAndSend("login",login1);
+        lp.findAndSend("password",password1);
+    }
+
+    @When("^click login button$")
+    public void clickLoginButton()
+    {
+        lp.findAndClick("loginButton");
+        lp.findAndClick("warningButton");
+        lp.findAndClick("warningLink");
+    }
+
+    @Then("^Verify that you are logged in\\.$")
+    public void verifyThatYouAreLoggedIn()
+    {
+        String expected="http://zero.webappsecurity.com/bank/account-summary.html";
+        String actual= BaseDriver.getDriver().getCurrentUrl();
+        Assert.assertEquals(expected,actual);
+    }
+}
---
+++ src/test/java/Utilities/BaseDriver.java
@@ -0,0 +1,30 @@
+package Utilities;
+
+import Pages.Parent;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseDriver {
+
+    public static WebDriver driver;
+
+    public static WebDriver getDriver() {
+
+        if (driver == null) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        }
+        return driver;
+    }
+
+    public static void DriverQuit() {
+
+        Parent.waiting(5);
+
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+}
